d6cff22bf8f991559f1c113d47d6db277f26cc12
admin <vageesh.v.bhat@gmail.com>
2023-07-16 13:50:38 -0700

    Auto push

--- example/provider.tf
+++ example/provider.tf
@@ -1,7 +1,9 @@
-# provider "aws" {}
+provider "aws" {}
 
-provider "aws" {
-  shared_credentials_files = "/Users/me/.aws/credentials"
+/* provider "aws" {
+  shared_credentials_file = "/Users/me/.aws/credentials"
   profile                 = "default"
   region                  = "us-east-1"
 }
+
+*/
