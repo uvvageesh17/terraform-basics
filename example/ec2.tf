4c735541d33b4dacab2700e023a6f6e998169bb3
admin <vageesh.v.bhat@gmail.com>
2023-07-16 13:39:43 -0700

    Auto push

--- example/ec2.tf
+++ example/ec2.tf
@@ -1,12 +1,21 @@
 #ami-0c1d144c8fdd8d690
 
-resource "aws_instance" "web" {
-  ami = "ami-0c1d144c8fdd8d690"
-  instance_type = "t2.micro"
+# resource "aws_instance" "web" {
+#   ami = "ami-0c1d144c8fdd8d690"
+#   instance_type = "t2.micro"
 
   
-tags = {
-    Name = "Hello-world"
-}
+# tags = {
+#     Name = "Hello-world"
+# }
 
+# }
+
+resource "aws_instance" "app" {
+  ami           = "ami-0c1d144c8fdd8d690"
+  instance_type = "t3.nano"
+
+  tags = {
+    Name = "TerraformServer"
+  }
 }
