8c8cc52e1132007560e7c915533acacb772bb484
admin <vageesh.v.bhat@gmail.com>
2023-07-20 21:29:36 -0700

    Auto push

--- 07-modules/backend.tf
+++ 07-modules/backend.tf
@@ -1,8 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "b54--terraform-remote-state"
-    key            = "05-remote/terraform.tfstate"     # This is like the folder path that you prefer giving
-    region         = "us-east-1"
+    key            = "05-remote/terraform.tfstate"     
   }
 }
 
