48cffeffa31e63a92bca680f2e0bcf0e6d4cfcb2
admin <vageesh.v.bhat@gmail.com>
2023-07-20 21:35:01 -0700

    Auto push

--- 07-modules/ec2/data.tf
+++ 07-modules/ec2/data.tf
@@ -1,5 +1,5 @@
 data "aws_ami" "image" {
   most_recent      = true
-  name_regex       = "ami-082237bea26382803"
-  /* owners          = ["self"] */
+  name_regex       = "DevOps-LabImage-CentOS7"
+  owners          = ["self"]
 }
